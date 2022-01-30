6cd5928fe2285f3fb887b2597d73427613d63c70
EljoPleqi <89164482+EljoPleqi@users.noreply.github.com>
2022-01-30 17:11:33 +0100

    initiated cart sectior

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ShoppingCartIcon } from '@heroicons/react/outline';
 import { Link, Router } from 'react-router-dom';
 
 const Header = () => {
@@ -10,19 +11,19 @@ const Header = () => {
       <div className="navigation flex  ">
         <ul className="flex ">
           <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
-            <Link to="signup">Sign up</Link>
-          </li>
-          <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
             nav-item
           </li>
           <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
             nav-item
           </li>
-          <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
-            nav-item
+          <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer flex gap-2">
+            Cart <ShoppingCartIcon className="w-6 h-6" />
           </li>
           <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
             <Link to="/Login">Log In</Link>
+          </li>
+          <li className="px-5 text-xl text-333 hover:text-orange-500 active:text-green-600 cursor-pointer">
+            <Link to="signup">Sign up</Link>
           </li>
         </ul>
       </div>
